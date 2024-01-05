9804a94458b4e5e8839f9c022cb22576f67ac566
Shiva Nandamuri <snandamu@MAC-K7YJ7G4QXH.local>
2024-01-05 16:11:39 -0500

    Update data types

--- Models/Availability.cs
+++ Models/Availability.cs
@@ -2,8 +2,8 @@ namespace AirbnbGrpc.Models;
 
 public class Availability
 {
-    public string availability_30 { get; set; }
-    public string availability_60 { get; set; }
-    public string availability_90 { get; set; }
-    public string availability_365 { get; set; }
+    public int availability_30 { get; set; }
+    public int availability_60 { get; set; }
+    public int availability_90 { get; set; }
+    public int availability_365 { get; set; }
 }
--- Models/Location.cs
+++ Models/Location.cs
@@ -3,6 +3,6 @@ namespace AirbnbGrpc.Models;
 public class Location
 {
     public string type { get; set; }
-    public List<string> coordinates { get; set; }
+    public List<double> coordinates { get; set; }
     public bool is_location_exact { get; set; }
 }
